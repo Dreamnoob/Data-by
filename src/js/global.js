3751031257851e2ec0043c33ef700323d8b7d1bf
Rostislav <nehaiki@ukr.net>
2022-01-12 13:56:42 +0200

    add partners

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    let blockItem = document.querySelectorAll('.block-item');
 
+    blockItem.forEach(item => {
+
+        let descr = item.querySelector('.block-item__descr');
+        let descrHeight = descr.scrollHeight;
+
+        item.addEventListener('mouseenter', function () {
+            descr.style.maxHeight = descrHeight + 'px';
+            item.classList.add('active');
+        });
+
+        item.addEventListener('mouseleave', function () {
+            descr.style.maxHeight = 0;
+            setTimeout(function () {
+                item.classList.remove('active');
+            }, 300);
+        });
+
+
+    });
 
 });
